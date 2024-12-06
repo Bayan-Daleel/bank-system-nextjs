89a1e27ad0cce14eb8520c07964a7bc84ef0b9e4
Bayan-Daleel <bayan.dl96@gmail.com>
2024-12-06 15:26:26 +0300

    end of navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const logIn ={firstname :'Bayan'}
   return (
-    <div>
-      home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+          type='greeting'
+          title='welcome'
+          user={logIn && logIn.firstname || 'Bayan'} 
+          subtext='access and manage your account and translations effects'
+           />
+           <TotalBalanceBox
+           accounts={[]} 
+           totalBanks={1}
+           totalCurrentBalance={1250.25}
+           />
+      </header>
+      </div>
+    </section>
   )
 }
 
---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}:{amount:number}) => {
+  return (
+    <div className='w-full'>
+        <CountUp end={amount}
+        separator=","
+        decimals={2}
+        prefix="$"/>
+    </div>
+  )
+}
+
+export default AnimatedCounter
